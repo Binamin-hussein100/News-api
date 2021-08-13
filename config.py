81bc7942b9bc2f9d8574359effe5bf2e2e90ccc8
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-08-13 14:38:49 +0300

    Added api key to config

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config:
    
-    SOURCE_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}&language=en'
-    ARTICLES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    NEWS_API_KEY = 'e2ae393fb095455989ead1e0dea06939'
+    # SOURCE_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}&language=en'
+    # ARTICLES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    # NEWS_API_KEY = 'e2ae393fb095455989ead1e0dea06939'
     
 class ProdConfig(Config):
      pass
